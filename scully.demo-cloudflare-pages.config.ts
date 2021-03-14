f0a8464b1758210f43b377f22a0d0a1c1cac69ea
Jefiozie <jefiozie.bosch@gmail.com>
2021-03-14 21:23:04 +0100

    ci: add the last parts for cloudflare

--- scully.demo-cloudflare-pages.config.ts
+++ scully.demo-cloudflare-pages.config.ts
@@ -4,5 +4,21 @@ export const config: ScullyConfig = {
   projectName: "demo-cloudflare-pages",
   outDir: './dist/static',
   routes: {
-  }
+  },
+  puppeteerLaunchOptions: {
+    args: [
+      '--disable-gpu',
+      '--renderer',
+      '--no-sandbox',
+      '--no-service-autorun',
+      '--no-experiments',
+      '--no-default-browser-check',
+      '--disable-dev-shm-usage',
+      '--disable-setuid-sandbox',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
+      '--disable-extensions',
+    ],
+  },
 };
