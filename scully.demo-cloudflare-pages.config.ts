be6fd1612a5e42a3ae7d65985410dcae4aa55bf5
Jefiozie <jefiozie.bosch@gmail.com>
2021-03-14 20:48:20 +0100

    initial commit

---
+++ scully.demo-cloudflare-pages.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "demo-cloudflare-pages",
+  outDir: './dist/static',
+  routes: {
+  }
+};
